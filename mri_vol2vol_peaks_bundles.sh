8fe561d59a2adf6bab8dd2667283bd17dcda7e21
Brad Caron <bacaron245@gmail.com>
2023-11-05 01:52:23 -0500

    Update mri_vol2vol_peaks_bundles.sh

--- mri_vol2vol_peaks_bundles.sh
+++ mri_vol2vol_peaks_bundles.sh
@@ -4,7 +4,7 @@ peaks=`jq -r '.peaks' config.json`
 bundles=`jq -r '.masks' config.json`
 t1=`jq -r '.t1' config.json`
 
-mkdir peaks masks
+mkdir peaks masks masks/masks
 export SUBJECTS_DIR=./
 mri_vol2vol --mov ${peaks} --targ ${t1} --regheader --interp nearest --o peaks_t1.nii.gz
 mri_vol2vol --mov ${bundles}/mask.nii.gz --targ ${t1} --regheader --interp nearest --o bundles_t1.nii.gz
