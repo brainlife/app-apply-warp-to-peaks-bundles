5594e7ed063681c6e9502df473f6ec6f64d00557
bacaron <bacaron245@gmail.com>
2023-11-05 01:37:05 -0500

    reason: dumb fix

--- mri_vol2vol_peaks_bundles.sh
+++ mri_vol2vol_peaks_bundles.sh
@@ -5,6 +5,6 @@ bundles=`jq -r '.masks' config.json`
 t1=`jq -r '.t1' config.json`
 
 mkdir peaks bundles
-
+export SUBJECTS_DIR=./
 mri_vol2vol --mov ${peaks} --targ ${t1} --regheader --interp nearest --o peaks_t1.nii.gz
 mri_vol2vol --mov ${bundles}/mask.nii.gz --targ ${t1} --regheader --interp nearest --o bundles_t1.nii.gz
